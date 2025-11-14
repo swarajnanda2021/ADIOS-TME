78217fb32b15cef450c24ef1e380d3a44b8430ab
swarajnanda2021 <swaraj.nanda.2019@gmail.com>
2025-11-14 18:56:09 -0500

    small bug fix

--- losses/correspondence_loss.py
+++ losses/correspondence_loss.py
@@ -191,7 +191,6 @@ class FeatureCorrespondenceLoss(nn.Module):
         )
         bg_features, bg_weights = self.select_top_k_patches(
             features, background_mask
-        )
         
         # Alignment losses (contrastive)
         L_nuclei = self.contrastive_alignment(
